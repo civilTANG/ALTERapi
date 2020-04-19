1036b828a9d10cc91692589b67d7e0b82edd25d4
civilTANG <45486293+civilTANG@users.noreply.github.com>
2020-04-19 20:48:43 +0800

    Update test.py

--- alterapi/test.py
+++ alterapi/test.py
@@ -1,3 +1,8 @@
-import api_replacement
-x = api_replacement.APIReplace('code.py')
-x.find('code.py')
+import alterapi
+print("static mode")
+x = alterapi.APIReplace('code.py'，option= 'static' )
+x.recommend()
+
+print("dynamic mode")
+x = alterapi.APIReplace('code.py'，option= 'dynamic' )
+x.recommend()
